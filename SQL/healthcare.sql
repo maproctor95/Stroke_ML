f5cefaa8189b586c9d4d922c85af0789504305b1
lafnggl <114120881+lafnggl@users.noreply.github.com>
2023-03-28 18:39:49 -0400

    SQL database

---
+++ SQL/healthcare.sql
@@ -0,0 +1,15 @@
+-- Create a new table
+CREATE TABLE healthcare (
+	id INT,
+	gender VARCHAR,
+	age VARCHAR,
+	hypertension INT, 
+	heart_disease INT, 
+	ever_married VARCHAR,
+	work_type VARCHAR,
+	Residence_type VARCHAR,
+	avg_glucose_level VARCHAR,
+	bmi VARCHAR,
+	smoking_status VARCHAR,
+	stroke INT
+);
